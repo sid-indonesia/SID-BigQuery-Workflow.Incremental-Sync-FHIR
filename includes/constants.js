c7b71049145e13c8bdccf666e0e5c38856db5a63
Muhammad Risky Pahlevi <muhammad.levi@sid-indonesia.org>
2024-11-07 09:16:09 +0000

    Published

--- includes/constants.js
+++ includes/constants.js
@@ -2,10 +2,12 @@ const datasetName = 'fhir_dataset_multiple_sources';
 const paramTimestampSubIntervalIntegerValue = 60;
 const paramTimestampSubIntervalDatePartValue = 'MINUTE';
 const startTimestamp = new Date().toISOString();
+const workflowName = 'Incremental Sync FHIR';
 
 module.exports = {
   datasetName,
   paramTimestampSubIntervalIntegerValue,
   paramTimestampSubIntervalDatePartValue,
   startTimestamp,
+  workflowName,
 };
